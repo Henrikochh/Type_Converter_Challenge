478d5916714b59284f32c09968ad3382e7a41daf
Henrikochh <113605484+Henrikochh@users.noreply.github.com>
2023-06-01 11:19:07 +0200

    update 2

---
+++ Type_Converter_Challenge/src/test/java/TypeConverterTest.java
@@ -0,0 +1,14 @@
+import ch.noseryoung.main.SideCreatorService;
+import ch.noseryoung.main.Triangle;
+import ch.noseryoung.main.TriangleManager;
+import ch.noseryoung.main.TriangleValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class TypeConverterTest {
+    @Test
+    public void test(){
+
+    }
+
+}
