7f0922fbe3850bd182fe17bcb171a6350b9ee8c8
Valentin Nussbaumer <valentin.nussbaumer@edu.tbz.ch>
2023-06-01 14:47:07 +0200

    added main

--- Type_Converter_Challenge/src/main/java/org/Main.java
+++ Type_Converter_Challenge/src/main/java/org/Main.java
@@ -2,6 +2,8 @@ package org;
 
 public class Main {
     public static void main(String[] args) {
+        TypeConverter converter = new TypeConverter();
+        converter.convertStringToShort("100");
 
     }
 }
